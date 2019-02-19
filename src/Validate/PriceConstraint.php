055375611016843e0ca8fa83331fc54d4aafec6a
enzo - Eduardo Garcia <enzo@weknowinc.com>
2019-02-19 19:07:48 +1100

    fix lint error

--- src/Validate/PriceConstraint.php
+++ src/Validate/PriceConstraint.php
@@ -14,7 +14,7 @@ class PriceConstraint {
    *
    * @param array $element
    *   The form element to process.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   * @param \Drupal\Core\Form\FormStateInterface $formState
    *   The form state.
    * @param array $form
    *   The complete form structure.
